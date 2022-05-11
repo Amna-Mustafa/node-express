40bcf58b57a625532fc3396e9a50c38264594343
Amna Mustafa <amnamustfa@MP15-AmnaM-SE-065.local>
2022-05-11 18:03:55 +0500

    Worked on weather API

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(err, req, res, next) {
   // });
 
   var XLSX = require('xlsx')
-  var workbook = XLSX.readFile("./master.xlsx");
+  var workbook = XLSX.readFile("./public/master.xlsx");
   var sheet_name_list = workbook.SheetNames;
   var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
   console.log(xlData);
---
+++ weather/app.js
@@ -0,0 +1,33 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var weather    = require('openweather-apis');
+
+
+//init app
+var app = express();
+
+//set the template engine
+app.set('view engine','ejs');
+
+//fetch the data from request
+app.use(bodyParser.urlencoded({extended:false}));
+
+//default pageload
+app.get('/',function(req,res){
+    res.render('home',{temp:null});
+});
+
+app.post('/',function(req,res){
+    
+    weather.setCity(req.body.city);
+    weather.setAPPID('41b7de0db756667afd8c5972e7205d88');
+    weather.getAllWeather(function(err,temp){
+        console.log(temp);
+        res.render('home',{temp:temp});
+    });
+});
+
+var port  = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log('server running at '+port);
+});
